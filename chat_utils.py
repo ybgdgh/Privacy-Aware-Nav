51e1273f20232e8965b391eb7df4219b17f054ff
ybgdgh <yubangguosdu@gmail.com>
2025-02-24 00:29:35 +0100

    update path generation

--- chat_utils.py
+++ chat_utils.py
@@ -2,61 +2,130 @@ import numpy as np
 import ast
 import jsonschema
 from jsonschema import validate
-from utils.json_validate import schema
 import time
 import requests
 import json
-from arguments import get_args
-
-
-gpt_name = [
-            'text-davinci-003',
-            'gpt-3.5-turbo-0125',
-            'gpt-4o',
-            'gpt-4o-mini'
-        ]           
-
-args = get_args()
-
+import base64
 import openai
-from openai.error import OpenAIError
-api_key = "xx-xxx"
+from openai import OpenAI
+from io import BytesIO
+from PIL import Image
+
+import system_prompt
+
+client = OpenAI()
+
+def message_prepare(prompt, candidate_map_list, navigation_instruct):
+    base64_image_list = []
+    for image_candidate in candidate_map_list:
+        base64_image_list.append(base64.b64encode(image_candidate.getvalue()).decode("utf-8"))
 
 
+    message = []
+    message.append({"role": "system", "content": prompt})
 
-def chat_with_gpt4v(chat_history, gpt_type = args.gpt_type):
+    image_contents = []
+    image_contents.append({
+        "type": "text",
+        "text": navigation_instruct,
+    })
+    for base64_image in base64_image_list:
+        image_contents.append({
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+        })
+    message.append({"role": "user", "content": image_contents})
     
-    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+    return message
+
+def generate_message_prepare(sys_prompt, map, navigation_instruct):
+
+    message = []
+    message.append({
+        "role": "system", 
+        "content": sys_prompt,
+    })
+    
+    image_path = "/home/ybg/Project/Privacy-Aware-Nav/img/3_example.PNG"
+    pil_image = Image.open(image_path)
+    buffered = BytesIO()
+    pil_image.save(buffered, format="JPEG")
+    base64_image_1 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    
+    example_contents = []
+    example_contents.append({
+        "type": "text",
+        "text": system_prompt.generate_path_examp_1,
+    })
+    example_contents.append({
+        "type": "image_url",
+        "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image_1}"
+        }
+    })
+    example_contents.append({
+        "type": "text",
+        "text": system_prompt.generate_path_examp_2,
+    })
+    
+    
+    image_contents = example_contents
+    image_contents.append({
+        "type": "text",
+        "text": navigation_instruct,
+    })
+    base64_image = base64.b64encode(map.getvalue()).decode("utf-8")
+    image_contents.append({
+        "type": "image_url",
+        "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+        }
+    })
+    message.append({"role": "user", "content": image_contents})
+    
+    return message
+
+def chat_with_gpt4v(chat_history):
     retries = 10    
     while retries > 0:  
         try: 
-            payload = {
-                "model": 'gpt-4o',
-                "response_format": { "type": "json_object" },
-                "temperature": 1.1,
-                "messages": chat_history,
-                "max_tokens": 100
-            }
-            
-            response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+            response = client.chat.completions.create(
+                model='o1', 
+                response_format = { "type": "json_object" },
+                messages = chat_history,
+                # temperature=0.1,
+                # max_tokens=300,
+            )
 
-            response_message = response.json()["choices"][0]["message"]["content"]
-            # print(response_message)
+            response_message = response.choices[0].message.content
+            print('gpt-4o' + " response: ")
+            print(response_message)
             try:
                 ground_json = ast.literal_eval(response_message)
-                break
+
+                return ground_json
                 
             except (SyntaxError, ValueError) as e:
-                print(e)
-        except OpenAIError as e:
-            if e:
-                print(e)
-                print('Timeout error, retrying...')    
-                retries -= 1
-                time.sleep(5)
-            else:
-                raise e
+                print(response_message)
+        except openai.APIError as e:
+            #Handle API error here, e.g. retry or log
+            print(f"OpenAI API returned an API Error: {e}")
+            pass
+        except openai.APIConnectionError as e:
+            #Handle connection error here
+            print(f"Failed to connect to OpenAI API: {e}")
+            pass
+        except openai.RateLimitError as e:
+            #Handle rate limit error (we recommend using exponential backoff)
+            print(f"OpenAI API request exceeded rate limit: {e}")
+            pass
+        retries -=1
             
-    print(response_message)
-    return response_message
+    # print(ground_json)
+    ground_json = {
+                    "path": "",
+                    }
+    return ground_json
 
--- system_prompt.py
+++ system_prompt.py
@@ -1,4 +1,4 @@
-prompt = """You are a mobile robot responsible for selecting the optimal path based on the attributes of the transporting objects, the features of the environment, and the influence of human activaty. 
+infer_prompt = """You are a mobile robot responsible for selecting the optimal path based on the attributes of the transporting objects, the features of the environment, and the influence of human activaty. 
 
 Your task involves analyzing several top-view maps of the scene, each illustrating different potential pathways from a starting point to the destination. Your goal is to choose the optimal path that best aligns with the needs of the attributes of the special object in terms of privacy and safety, the features of the scene, and the possible influence of human activaty in the scene such as avoid passing though the office area to decrease the potential influence of the human work, apart from just considering the shortest distance. Let’s think step by step.
 
@@ -15,3 +15,46 @@ Output Example:
 """
 
 
+generate_path = """You are a path planing agent that responsible for selecting the optimal path based on the attributes of the transporting objects, the features of the environment, and the influence of human activaty. 
+
+Your task involves analyzing the given top-view map of the scene with random landmarks on the map. The starting point (S) to the destination (D) are also marked on the map. Your goal is to choose the landmarks as a path from start (S) to destination (D) that best aligns with the needs of the attributes of the special object in terms of privacy and safety. Since the path is used for robot navigation, you can only choose the landmarks on the main corridors since the robot can't cross the other rooms. The black areas on the map are impassable. Let's think step by step.
+
+- Input: You will receive the description of the object needing transportation and a top-view map of the scene which marked with random landmarks. 
+- Output: Your response should be a JSON object indicating the landmark ID you believe is most suitable given the object's specific requirements.
+
+Output format:
+{
+    "observation": "Analyze what kind of scenes in the top-view maps and all the possible topological paths from start to destination", 
+    "plan": "find a way that best aligns with the needs of the task using the landmarks"
+    "reason": "Analyze which path should be selected and why don't select other paths",
+    "path": "s, ..., d"
+    "self-critique": "constructive self-critique for the alignments with the nned of the task, and check the landmarks of the path quality that avoid repeating and meanless detour.",
+}
+
+There is an example: 
+
+"""
+generate_path_examp_1 = """
+Example:
+
+Input:
+Instruction: send a classified file from an office to another office.
+Input top-view map:
+
+"""
+generate_path_examp_2 = """
+Output:
+{
+    "observation": "There are few office rooms, conferece rooms, bathrooms, and corridors. The start is a office, and the destination is a conference room. It looks lile there are two ways from the start to destination. The upper way would pass a large lounge and many empty corridors, the down loop path is more shorter but will pass many office areas.", 
+    "plan": "I need to find a way using the landmarks on the map. I can only select the landmarks on the hallways that can be used to connect and represent the path from start to end room and avoid obstales of the wall and room.",
+    "reason": "The upper way is longer than the dowm way, but pass less office and conference area, which can keep the safety and privacy of the classified file for transportation",
+    "path": "s, 5, 32, 31, 15, 30, 19, d"
+    "self-critique": "The chosen path minimizes exposure to potential hazards and avoids unnecessary detours, all the landmarks are in the hallways, not in the other rooms",
+}
+
+please give the output based on the following input massages:
+
+"""
+
+x = """
+generate_path_examp_1 = """
